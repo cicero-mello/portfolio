658f7cf1a7ce03b01c729d742bbef8c489d44298
Cícero Mello <ciceromello.dev@gmail.com>
2025-05-21 19:38:15 -0300

    🩹 🚀 Fixing Image Import

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,15 +1,16 @@
+import Image from '../../../assets/images/me-brown.webp?w=3017&h=3017&jsx'
 import { component$ } from "@builder.io/qwik"
-import * as S from "./styles.css"
 import { Background } from "./background"
+import * as S from "./styles.css"
 
 export const Hero = component$(() => {
 
     return (
         <S.Content>
             <S.Card>
-                <S.Image
-                    src="/assets/images/me-brown.webp"
-                />
+                <S.ImageButton>
+                    <Image class={S.imageClass} />
+                </S.ImageButton>
                 <S.Text>
                     Hi<br />
                     I'm Cícero<br />
--- src/components/hero/styles.css.ts
+++ src/components/hero/styles.css.ts
@@ -1,4 +1,5 @@
 import { styled } from "styled-vanilla-extract/qwik"
+import { style } from "@vanilla-extract/css"
 
 export const Content = styled.div`
     display: flex;
@@ -16,17 +17,23 @@ export const Card = styled.div`
     padding-bottom: 2rem;
 `
 
-export const Image = styled.img`
+export const ImageButton = styled.button`
+    border-radius: 50%;
+    display: flex;
+    overflow: hidden;
     max-width: 15rem;
     width: 100%;
-    border-radius: 50%;
-    user-select: none;
 
     // 0 4px 4px
     filter: drop-shadow(
         0 0.25rem 0.25rem rgba(0, 0, 0, 0.25)
     );
 `
+
+export const imageClass = style({
+    width: "100%",
+    height: "auto"
+})
 
 export const Text = styled.p`
     font-size: 2.5rem;
