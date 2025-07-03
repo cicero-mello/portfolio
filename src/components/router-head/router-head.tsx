a812f484f5939f1cc3b9e9bb7416d3c15d85e9eb
Cícero Mello <ciceromello.dev@gmail.com>
2025-07-02 22:13:05 -0300

    🍱 Using Only PNG Favicon (change related to google search)

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,17 +16,10 @@ export const RouterHead = component$(() => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link
                 rel="icon"
-                type="image/svg+xml"
-                href="https://www.ciceromello.com/favicon.svg"
-                sizes="150x150"
-            />
-            <link
-                rel="icon"
                 type="image/png"
-                sizes="96x96"
+                sizes="150x150"
                 href="https://www.ciceromello.com/favicon.png"
             />
-
             {head.meta.map((m) => (
                 <meta key={m.key} {...m} />
             ))}
